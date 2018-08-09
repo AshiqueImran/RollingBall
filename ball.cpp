f06c028b172df34ca536897f13dc3e1665cdc68c
abrarShariar <shariarabrar@gmail.com>
2018-08-09 17:00:42 +0600

    added rand objects

--- ball.cpp
+++ ball.cpp
@@ -16,10 +16,10 @@ void initRendering() {
 
 
 //random number generator
-float getRamdomNum(float min, float max){
+float getRamdomNum(float min, float max, float step){
 	srand(time(0));
 	float range = (max - min);
-	float rand_num = min + float((range * rand()) / (RAND_MAX +  1.0));
+	float rand_num = min + float((range * rand()) / (RAND_MAX +  step));
 	return rand_num;
 }
 
@@ -1398,6 +1398,8 @@ float _obj_color_r = 1.0;
 float _obj_color_g = 0.0;
 float _obj_color_b = 0.0;
 float _obj_pos_y = -0.5;
+float _obj_radius_in = 0.2;
+float _obj_radius_out = 0.5;
 
 void buildAndRollObjects(){
 	glPushMatrix();
@@ -1405,10 +1407,17 @@ void buildAndRollObjects(){
 			glTranslatef(_move_o1, 0.0, 0.0);
 			glTranslatef(0.0, _obj_pos_y, 0.0);
 			glRotatef(-90, 1.0, 0.0, 0.0);
-			// glRotatef(_angle, 0.0, 0.0, 1.0);
-
-			glutWireTorus(0.2, 0.3, 40, 40);
+			glRotatef(_angle, 0.0, 0.0, 1.0);
+			glutWireTorus(_obj_radius_in, _obj_radius_out, 20, 40);
 	glPopMatrix();
+
+	glPushMatrix();
+			 glColor3f(1.0, 1.0, 1.0);
+			 glTranslatef(0.0, 3.0, -1.0); //Move forward 5 units
+			 glRotatef(-90, 1.0, 0.0, 0.0);
+			 // glRotatef(_angle, 0.0, 0.0, 1.0);
+			 glutWireTorus(0.2, 0.9, 20, 20);
+	 glPopMatrix();
 }
 
 //Draws the 3D scene
@@ -1474,16 +1483,17 @@ void updateObjects(int value){
 	_move_o1 += 0.06f;
 	if(_move_o1 > 5.0){
 			_move_o1 = -4.5;
-			_obj_color_r = getRamdomNum(-1.0, 1.0);
-			_obj_color_g = getRamdomNum(-2.0, 2.0);
-			_obj_color_b = getRamdomNum(-2.0, 2.0);
-			_obj_pos_y = getRamdomNum(-0.5, -2.0);
+			_obj_color_r = getRamdomNum(-1.0, 1.0, 1.0);
+			_obj_color_g = getRamdomNum(-2.0, 2.0, 1.0);
+			_obj_color_b = getRamdomNum(-2.0, 2.0, 1.0);
+			_obj_pos_y = getRamdomNum(-0.5, -1.5, .5);
+			_obj_radius_in = getRamdomNum(0.1, 0.2, .05);
+			_obj_radius_out = getRamdomNum(0.2, 0.4, .05);
 	}
 	glutTimerFunc(15, updateObjects, 0);
 }
 
 int main(int argc, char** argv) {
-	std::cout<<getRamdomNum(3.0, 3.5)<<std::endl;
 
 	//Initialize GLUT
 	glutInit(&argc, argv);
